ba290180ab402b2c5dc9632f2f51232757ff0b4f
Marcin <Marcin@Marcin>
2016-01-15 12:37:41 +0100

    up rest gra

--- src/main/java/services/GraFacadeREST.java
+++ src/main/java/services/GraFacadeREST.java
@@ -5,13 +5,8 @@
  */
 package services;
 
-import beansDB.GraBean;
-import beansDB.UzytkownikBean;
 import entitys.Gra;
-import entitys.Uzytkownik;
-import java.util.Date;
 import java.util.List;
-import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -23,24 +18,16 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Response;
 
 /**
  *
  * @author Marcin
  */
 @Stateless
-@Path("zagraj")
+@Path("entitys.gra")
 public class GraFacadeREST extends AbstractFacade<Gra> {
-
     @PersistenceContext(unitName = "WebApplication1PU")
     private EntityManager em;
-
-    @EJB
-    private UzytkownikBean uzytkownikBean;
-
-    @EJB
-    private GraBean graBean;
 
     public GraFacadeREST() {
         super(Gra.class);
@@ -48,37 +35,16 @@ public class GraFacadeREST extends AbstractFacade<Gra> {
 
     @POST
     @Override
-    @Consumes({"application/json"})
+    @Consumes({"application/xml", "application/json"})
     public void create(Gra entity) {
         super.create(entity);
     }
 
     @PUT
     @Path("{id}")
-    @Consumes({"application/json"})
+    @Consumes({"application/xml", "application/json"})
     public void edit(@PathParam("id") Long id, Gra entity) {
-        entity.setId(id);
         super.edit(entity);
-    }
-
-    @PUT
-    @Path("/zapiszuzytkownika/{wybor}")
-    @Consumes({"application/json"})
-    public Response zapiszUzytkownika(@PathParam("wybor") Integer wybor, Uzytkownik user) {
-        System.out.println("OK");
-        Gra entity = new Gra();
-        entity = graBean.wezGraByData(new Date());
-        if (wybor == 0) {
-            entity.getListaChetnych().add(user);
-        } else if (wybor == 1) {
-            entity.getListaMoze().add(user);
-        } else {
-            entity.getListaNie().add(user);
-        }
-        System.out.println("OK");
-        super.edit(entity);
-
-        return Response.status(Response.Status.OK).build();
     }
 
     @DELETE
@@ -87,81 +53,25 @@ public class GraFacadeREST extends AbstractFacade<Gra> {
         super.remove(super.find(id));
     }
 
-    @DELETE
-    @Path("wypiszuzytkownika/{id}/{wybor}")
-    public Response wypiszUzytkownika(@PathParam("id") Long id,@PathParam("wybor") Integer wybor) {
-        Long idGra = graBean.wezGraByData(new Date()).getId();
-        if (wybor == 0) {
-            graBean.usunChętnego(uzytkownikBean.getUserById(id), idGra);
-        } else if (wybor == 1) {
-            graBean.usunDecyzje(uzytkownikBean.getUserById(id), idGra, wybor);
-        } else {
-            graBean.usunDecyzje(uzytkownikBean.getUserById(id), idGra, wybor);
-        }
-        return Response.status(Response.Status.OK).build();
-    }
-
     @GET
     @Path("{id}")
-    @Produces({"application/json"})
+    @Produces({"application/xml", "application/json"})
     public Gra find(@PathParam("id") Long id) {
         return super.find(id);
     }
 
-    
-    @GET
-    @Path("/getgra")
-    @Produces({"application/json"})
-    public Response getGra() {
-        Gra gra= new Gra();
-        if(!graBean.sprawdzCzyGraja(new Date())){
-            gra.setData(new Date());
-            gra.setDodatkoweInformacje("dotatkowe info");
-            graBean.add(gra);
-        }
-        gra = graBean.wezGraByData(new Date());
-        return Response.ok(gra).build();
-    }
-
     @GET
     @Override
-    @Produces({"application/json"})
+    @Produces({"application/xml", "application/json"})
     public List<Gra> findAll() {
         return super.findAll();
     }
 
     @GET
     @Path("{from}/{to}")
-    @Produces({"application/json"})
+    @Produces({"application/xml", "application/json"})
     public List<Gra> findRange(@PathParam("from") Integer from, @PathParam("to") Integer to) {
         return super.findRange(new int[]{from, to});
-    }
-    @GET
-    @Path("/listachetnych")
-    @Produces({"application/json"})
-    public List<Uzytkownik> listaChetnych() {
-        Gra gra = new Gra();
-        gra =graBean.wezGraByData(new Date());
-        
-        return gra.getListaChetnych();
-    }
-    @GET
-    @Path("/listaniezdecydowanych")
-    @Produces({"application/json"})
-    public List<Uzytkownik> listaNiezdecydowanych() {
-        Gra gra = new Gra();
-        gra =graBean.wezGraByData(new Date());
-        
-        return gra.getListaMoze();
-    }
-    @GET
-    @Path("/listanieobecni")
-    @Produces({"application/json"})
-    public List<Uzytkownik> listaNieobecni() {
-        Gra gra = new Gra();
-        gra =graBean.wezGraByData(new Date());
-        
-        return gra.getListaNie();
     }
 
     @GET
@@ -175,5 +85,5 @@ public class GraFacadeREST extends AbstractFacade<Gra> {
     protected EntityManager getEntityManager() {
         return em;
     }
-
+    
 }
